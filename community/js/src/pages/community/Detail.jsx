0fe4236fd9090a7b5f70ae65548fabcd09041a0e
s0zzang <dlth8888@gmail.com>
2024-07-09 14:47:38 +0900

    ✅ [FE] update: 게시글 상세 유저 프로필 추가, 작성글에만 수정/삭제 버튼 노출

--- community/js/src/pages/community/Detail.jsx
+++ community/js/src/pages/community/Detail.jsx
@@ -1,39 +1,51 @@
 import Button from "@components/Button";
-import useFetch from "../../hooks/useFetch";
+import useFetch, { API_SERVER } from "@hooks/useFetch";
 import { useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { userState } from "@recoil/atoms";
 import CommentList from "./CommentList";
 
 const Detail = () => {
   let { _id } = useParams();
   const { data } = useFetch(`/posts/${_id}`);
+  const user = useRecoilValue(userState);
 
   return (
     data && (
       <main className="container mx-auto mt-4 px-4">
         <section className="mb-8 p-4">
-          <div className="font-semibold text-xl">제목 : {data.title}</div>
-          <div className="text-right text-gray-400">
-            작성자 : {data.user.name}
-          </div>
-          <div className="mb-4">
-            <div>
-              <pre className="font-roboto w-full p-2 whitespace-pre-wrap">
-                {data.content}
-              </pre>
+          <div className="flex flex-col">
+            <div className="font-semibold text-xl">{data.title}</div>
+            <div className="flex items-center gap-x-1 ml-auto text-right text-gray-400">
+              <img
+                className="w-5 aspect-square object-cover rounded-full"
+                src={`${API_SERVER}/${data.user.profile}`}
+                alt=""
+              />
+              <p>{data.user.name}</p>
             </div>
-            <hr />
           </div>
-          <div className="flex justify-end my-4">
+          <div>
+            <pre className="font-roboto w-full py-8 whitespace-pre-wrap">
+              {data.content}
+            </pre>
+          </div>
+          <hr />
+          <div className="flex justify-end my-8">
             <Button onClick={() => history.back()}>목록</Button>
-            <Button
-              bgColor="gray"
-              onClick={() => (location.href = "/info/1/edit")}
-            >
-              수정
-            </Button>
-            <Button bgColor="red" onClick={() => (location.href = "/info")}>
-              삭제
-            </Button>
+            {user._id === data.user._id && (
+              <>
+                <Button
+                  bgColor="gray"
+                  onClick={() => (location.href = "/info/1/edit")}
+                >
+                  수정
+                </Button>
+                <Button bgColor="red" onClick={() => (location.href = "/info")}>
+                  삭제
+                </Button>
+              </>
+            )}
           </div>
         </section>
 
