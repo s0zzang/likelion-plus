8f3b7627686f249d4420641ededc16c51798781d
s0zzang <dlth8888@gmail.com>
2024-07-09 16:22:24 +0900

    ✅ [FE] update: 댓글 작성, 삭제

--- community/js/src/pages/community/CommentItem.jsx
+++ community/js/src/pages/community/CommentItem.jsx
@@ -1,5 +1,59 @@
-const CommentItem = () => {
-  return <div>CommentItem</div>;
+import Button from "@components/Button";
+import { API_SERVER } from "@hooks/useFetch";
+import { Link, useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { userState } from "@recoil/atoms";
+import useMutation from "@hooks/useMutation";
+
+const CommentItem = ({ item, refetch }) => {
+  const { _id } = useParams();
+  const user = useRecoilValue(userState);
+  const { send } = useMutation(`/posts/${_id}/replies/${item._id}`, {
+    method: "DELETE",
+  });
+  const profile = item.user.profile
+    ? `${API_SERVER}/${
+        item.user.profile instanceof Object
+          ? item.user.profile.path
+          : item.user.profile
+      }`
+    : "/favicon.png";
+
+  const handleDelete = () => {
+    send({
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token.accessToken}`,
+      },
+    });
+    refetch();
+  };
+
+  return (
+    <div className="shadow-md rounded-lg p-4 mb-4">
+      <div className="flex justify-between items-center mb-2">
+        <img
+          className="w-8 mr-2 rounded-full object-cover aspect-square"
+          src={profile}
+          alt={`${item.user.name} 프로필 이미지`}
+        />
+        <Link to="" className="text-orange-400">
+          {item.user.name}
+        </Link>
+        <time className="ml-auto text-gray-500" dateTime={item.createdAt}>
+          {item.createdAt}
+        </time>
+      </div>
+      <div className="flex justify-between items-center mb-2">
+        <pre className="whitespace-pre-wrap text-sm">{item.content}</pre>
+        {user._id === item.user._id && (
+          <Button bgColor="red" size="sm" onClick={handleDelete}>
+            삭제
+          </Button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CommentItem;
--- community/js/src/pages/community/CommentList.jsx
+++ community/js/src/pages/community/CommentList.jsx
@@ -1,45 +1,23 @@
-import useFetch, { API_SERVER } from "../../hooks/useFetch";
-import Button from "@components/Button";
+import useFetch from "@hooks/useFetch";
 import CommentNew from "./CommentNew";
+import CommentItem from "./CommentItem";
 
-const CommentList = ({ _id }) => {
+const CommentList = ({ _id, refetch }) => {
   const { data } = useFetch(`/posts/${_id}/replies`);
-  console.log(data);
 
   return (
     <section className="mb-8">
       <h4 className="mt-8 mb-4 ml-2">댓글 {data?.length || 0}개</h4>
 
       {/* 댓글 */}
-      {data?.length ? (
-        <div className="shadow-md rounded-lg p-4 mb-4">
-          <div className="flex justify-between items-center mb-2">
-            <img
-              className="w-8 mr-2 rounded-full object-cover aspect-square"
-              src={`${API_SERVER}/${data[0].user.profile}`}
-              alt={`${data[0].user.name} 프로필 이미지`}
-            />
-            <a href="" className="text-orange-400">
-              {data[0].user.name}
-            </a>
-            <time
-              className="ml-auto text-gray-500"
-              dateTime={data[0].createdAt}
-            >
-              {data[0].createdAt}
-            </time>
-          </div>
-          <div className="flex justify-between items-center mb-2">
-            <pre className="whitespace-pre-wrap text-sm">{data[0].content}</pre>
-            <Button bgColor="red" size="sm">
-              삭제
-            </Button>
-          </div>
-        </div>
-      ) : null}
+      {data?.length
+        ? data.map((item, idx) => (
+            <CommentItem item={item} key={idx} refetch={refetch} />
+          ))
+        : null}
 
       {/* 댓글 입력 */}
-      <CommentNew />
+      <CommentNew refetch={refetch} />
     </section>
   );
 };
