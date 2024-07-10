978c0e6822d2652fae0bef9279fbdb4162695f36
s0zzang <dlth8888@gmail.com>
2024-07-10 11:22:31 +0900

    🚧 [FE] cont: 게시판 아이템 인덱스 변경

--- community/js/src/pages/community/ListItem.jsx
+++ community/js/src/pages/community/ListItem.jsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
 
-const ListItem = ({ item, idx, type }) => {
+const ListItem = ({ item, type }) => {
   const navigate = useNavigate();
 
   return (
     <tr className="border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out">
-      <td className="p-2 text-center">{idx + 1}</td>
+      <td className="p-2 text-center">{item._id}</td>
       <td
         className="p-2 truncate indent-4 cursor-pointer"
         onClick={() => navigate(`/${type}/${item._id}`)}
