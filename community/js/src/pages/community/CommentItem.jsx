7e3f9228e33c7f36e8aa4b92537b26e4660df596
s0zzang <dlth8888@gmail.com>
2024-07-10 15:00:39 +0900

    ✅ [FE] update: 댓글 수정

--- community/js/src/pages/community/CommentItem.jsx
+++ community/js/src/pages/community/CommentItem.jsx
@@ -4,13 +4,13 @@ import { Link, useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { userState } from "@recoil/atoms";
 import useMutation from "@hooks/useMutation";
+import { useRef, useState } from "react";
 
 const CommentItem = ({ item, refetch }) => {
   const { _id } = useParams();
   const user = useRecoilValue(userState);
-  const { send } = useMutation(`/posts/${_id}/replies/${item._id}`, {
-    method: "DELETE",
-  });
+  const textareaRef = useRef();
+  const { send } = useMutation(`/posts/${_id}/replies/${item._id}`);
   const profile = item.user.profile
     ? `${API_SERVER}/${
         item.user.profile instanceof Object
@@ -18,6 +18,7 @@ const CommentItem = ({ item, refetch }) => {
           : item.user.profile
       }`
     : "/favicon.png";
+  const [isEdit, setIsEdit] = useState(false);
 
   const handleDelete = () => {
     send({
@@ -25,6 +26,20 @@ const CommentItem = ({ item, refetch }) => {
         "Content-Type": "application/json",
         Authorization: `Bearer ${user.token.accessToken}`,
       },
+      method: "DELETE",
+    });
+    refetch();
+  };
+
+  const handleEdit = () => {
+    const content = textareaRef.current.value;
+    send({
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user.token.accessToken}`,
+      },
+      body: JSON.stringify({ content }),
+      method: "PATCH",
     });
     refetch();
   };
@@ -45,11 +60,44 @@ const CommentItem = ({ item, refetch }) => {
         </time>
       </div>
       <div className="flex justify-between items-center mb-2">
-        <pre className="whitespace-pre-wrap text-sm">{item.content}</pre>
+        {isEdit ? (
+          <textarea
+            name="content"
+            id="content"
+            className="w-full rounded border-slate-400 border border-solid p-1 "
+            defaultValue={item.content}
+            ref={textareaRef}
+          ></textarea>
+        ) : (
+          <pre className="whitespace-pre-wrap text-sm">{item.content}</pre>
+        )}
+
         {user._id === item.user._id && (
-          <Button bgColor="red" size="sm" onClick={handleDelete}>
-            삭제
-          </Button>
+          <div className="flex-shrink-0">
+            {isEdit ? (
+              <>
+                <Button size="sm" onClick={handleEdit}>
+                  등록
+                </Button>
+                <Button
+                  bgColor="gray"
+                  size="sm"
+                  onClick={() => setIsEdit(!isEdit)}
+                >
+                  취소
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button size="sm" onClick={() => setIsEdit(!isEdit)}>
+                  수정
+                </Button>
+                <Button bgColor="red" size="sm" onClick={handleDelete}>
+                  삭제
+                </Button>
+              </>
+            )}
+          </div>
         )}
       </div>
     </div>
