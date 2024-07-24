7ccaced96e17315ab5fd39bc21b97607ec9f4f4e
s0zzang <dlth8888@gmail.com>
2024-07-24 16:00:44 +0900

    ✨ [FE] feat: 헤더 현재 메뉴 활성화

--- next.js/community/src/components/layout/Header.tsx
+++ next.js/community/src/components/layout/Header.tsx
@@ -1,7 +1,12 @@
+"use client";
 import Link from "next/link";
 import Anchor from "../Anchor";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+  const isActive = (path: string) => (pathname === path ? "cs-active" : "");
+
   return (
     <header className="px-8 min-w-80 bg-slate-100 dark:bg-gray-600 text-gray-800 dark:text-gray-200 transition-color duration-500 ease-in-out">
       <nav className="flex flex-wrap justify-center items-center p-4 md:flex-nowrap md:justify-between">
@@ -19,14 +24,33 @@ const Header = () => {
         </div>
         <div className="w-auto order-2 text-base mt-4 md:mt-0">
           <ul className="flex items-center gap-6 uppercase">
-            <li className="hover:text-amber-500 hover:font-semibold">
+            <li
+              className={`hover:text-amber-500 a:font-semibold ${isActive(
+                "/info"
+              )}`}
+            >
               <Link href="/info">정보공유</Link>
             </li>
-            <li className="hover:text-amber-500 hover:font-semibold">
+            <li
+              className={`hover:text-amber-500 a:font-semibold ${isActive(
+                "/free"
+              )}`}
+            >
               <Link href="/free">자유게시판</Link>
             </li>
-            <li className="hover:text-amber-500 a:font-semibold">
+            <li
+              className={`hover:text-amber-500 a:font-semibold ${isActive(
+                "/qna"
+              )}`}
+            >
               <Link href="/qna">질문게시판</Link>
+            </li>
+            <li
+              className={`hover:text-amber-500 a:font-semibold ${isActive(
+                "/notice"
+              )}`}
+            >
+              <Link href="/notice">공지게시판</Link>
             </li>
           </ul>
         </div>
